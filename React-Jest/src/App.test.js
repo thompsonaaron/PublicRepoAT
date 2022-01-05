ae9431347fd2952cba00997f159945aa0997f320
thompsonaaron <59658614+thompsonaaron@users.noreply.github.com>
2022-01-05 08:33:58 -0600

    button color testing

--- React-Jest/src/App.test.js
+++ React-Jest/src/App.test.js
@@ -1,12 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import App from "./App";
 
 // https://testing-library.com/docs/guide-which-query/
 // https://www.w3.org/TR/wai-aria/#role_definitions
 
-test("renders learn react link", () => {
+// test("renders learn react link", () => {
+// 	render(<App />);
+// 	const linkElement = screen.getByRole("link", { name: /learn react/i });
+// 	expect(linkElement).toBeInTheDocument();
+// });
+
+test("button has correct initial color and text", () => {
 	render(<App />);
-	const linkElement = screen.getByRole("link", { name: /learn react/i });
-	expect(linkElement).toBeInTheDocument();
+	const button = screen.getByRole("button", { name: "Change to blue" });
+	expect(button).toHaveStyle({ backgroundColor: "red" });
+
+	fireEvent.click(button);
+
+	expect(button).toHaveStyle({ backgroundColor: "blue" });
+	expect(button.textContent).toBe("Change to red");
 });
